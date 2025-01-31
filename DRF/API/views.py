443622ee1ea380a24a6a3139f357d70e3270dc1c
WabukoWabuko <basilwabbs@gmail.com>
2025-01-31 12:09:54 +0000

    Cofniguring and testing if it is working

---
+++ DRF/API/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import ArticleSerializer
+from .models import Article
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
+@api_view(['GET'])
+def article_list(request):
+  articles = Article.objects.all()
+  serializer = ArticleSerializer(articles, many=True)
+  return Response(serializer.data)
