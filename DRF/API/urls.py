a2976f66554d8781267aecf9055547601310927c
WabukoWabuko <basilwabbs@gmail.com>
2025-02-04 08:21:09 +0000

    Authentication of users

--- DRF/API/urls.py
+++ DRF/API/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework_simplejwt import views as jwt_views
 
 from .views import ArticleViewSet
 from rest_framework.routers import DefaultRouter
@@ -8,4 +9,6 @@ router.register(r'articles', ArticleViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token login
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), # Token refresh
 ]
--- DRF/API/views.py
+++ DRF/API/views.py
@@ -6,10 +6,12 @@ from rest_framework.decorators import APIView, api_view
 from rest_framework.views import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
   
